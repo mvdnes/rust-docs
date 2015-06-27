f43f9137af9307e8c60c26baab221116a81c505a
Mathijs van de Nes <git@mathijs.vd-nes.nl>
2015-06-27 13:28:51 +0200

    New attempt at travis doc upload

--- travis-doc-upload.sh
+++ travis-doc-upload.sh
@@ -1,28 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
 # License: CC0 1.0 Universal
 # https://creativecommons.org/publicdomain/zero/1.0/legalcode
 
 set -e
 
-SCRIPT_PATH=.travis
+SCRIPT_PATH=script
 
-. $SCRIPT_PATH/travis-doc-upload.cfg
+. $SCRIPT_PATH/doc-upload.cfg
 
-[ "$TRAVIS_BRANCH" = master ]
+[ "$TRAVIS_BRANCH" = master ] || exit 0
 
-[ "$TRAVIS_PULL_REQUEST" = false ]
+[ "$TRAVIS_PULL_REQUEST" = false ] || exit 0
 
-[ "$TRAVIS_RUST_VERSION" = "$DOC_RUST_VERSION" ]
+[ "$TRAVIS_RUST_VERSION" = "$DOC_RUST_VERSION" ] || exit 0
 
 echo "Publishing docs..."
 
-eval key=\$encrypted_${SSH_KEY_TRAVIS_ID}_key
-eval iv=\$encrypted_${SSH_KEY_TRAVIS_ID}_iv
+export DISPLAY=' '
+export SSH_ASKPASS=$HOME/ssh-askpass
+export GIT_SSH_COMMAND="setsid -w ssh -i $SCRIPT_PATH/travis-doc-upload.pem"
 
-mkdir -p ~/.ssh
-openssl aes-256-cbc -K $key -iv $iv -in $SCRIPT_PATH/id_rsa.enc -out ~/.ssh/id_rsa -d
-chmod 600 ~/.ssh/id_rsa
+cat << EOS > $HOME/ssh-askpass
+#!/bin/bash
+
+echo -n \$DEPLOY_KEY_PASS
+EOS
 
 git clone -q --branch gh-pages git@github.com:$DOCS_REPO deploy_docs
 
@@ -32,12 +35,16 @@ git config user.email "nobody@example.com"
 rm -rf $PROJECT_NAME
 mv ../target/doc $PROJECT_NAME
 git add -A $PROJECT_NAME
-git commit -m "doc upload for $PROJECT_NAME ($TRAVIS_REPO_SLUG)"
+git commit -m "doc upload for $PROJECT_NAME" || true
 
 for i in {1..5}; do
     git push -q origin gh-pages && break # if successful, break loop
     echo "Rebasing... attempt $i"
     git pull -q -r # otherwise try a rebase
 done
+
+unset DISPLAY
+unset SSH_ASKPASS
+unset GIT_SSH_COMMAND
 
 echo "Doc upload completed"
