80031000e1b31bf23c161a25334c53f6738f20e8
Mathijs van de Nes <git@mathijs.vd-nes.nl>
2015-06-28 11:18:58 +0200

    Use centralized doc upload key

--- travis-doc-upload.sh
+++ travis-doc-upload.sh
@@ -14,8 +14,10 @@ SCRIPT_PATH=script
 
 echo "Publishing docs..."
 
+# Get SSH key and decrypt it
+curl https://mvdnes.github.io/rust-docs/doc-upload-key.enc -o $HOME/doc-upload-key.enc
 mkdir -p $HOME/.ssh
-openssl aes-128-ofb -d -in $SCRIPT_PATH/travis-doc-upload.enc -out $HOME/.ssh/id_rsa -pass env:DEPLOY_KEY_PASS
+openssl aes-128-ofb -d -in $HOME/doc-upload-key.enc -out $HOME/.ssh/id_rsa -pass env:DEPLOY_KEY_PASS
 chmod 600 $HOME/.ssh/id_rsa
 
 git clone -q --branch gh-pages git@github.com:$DOCS_REPO deploy_docs
